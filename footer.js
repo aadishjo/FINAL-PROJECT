bf4f0948a86c86a17ea51b6b15ac7fcb10993b4b
aadishjo <141103426+aadishjo@users.noreply.github.com>
2023-08-07 19:59:26 -0400

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,7 @@
+// footer.js
+document.addEventListener("DOMContentLoaded", function () {
+    const footer = document.createElement("footer");
+    footer.innerHTML = "<p>&copy; " + new Date().getFullYear() + " Aadish DragonStrike Board Game. All rights reserved.</p>";
+    document.body.appendChild(footer);
+  });
+  
